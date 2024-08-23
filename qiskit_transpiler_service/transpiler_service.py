21dde9864f3fb2a9183e0900269ecbd8b91e931f
Jesus Talavera <jesus.talavera@ibm.com>
2024-08-23 17:37:22 +0200

    Remove redundant code in qiskit_transpiler_service by reusing the one from qiskit_ibm_transpiler

--- qiskit_transpiler_service/transpiler_service.py
+++ qiskit_transpiler_service/transpiler_service.py
@@ -12,10 +12,10 @@
 
 """
 ===============================================================================
-Qiskit IBM Transpiler (:mod:`qiskit_ibm_transpiler.transpiler_service`)
+Qiskit Transpiler Service (:mod:`qiskit_transpiler_service.transpiler_service`)
 ===============================================================================
 
-.. currentmodule:: qiskit_ibm_transpiler.transpiler_service
+.. currentmodule:: qiskit_transpiler_service.transpiler_service
 
 Classes
 =======
@@ -29,16 +29,14 @@ Classes
 import logging
 from typing import Dict, List, Union, Literal
 
-from qiskit import QuantumCircuit
-
-from .wrappers import TranspileAPI
+from qiskit_ibm_transpiler import TranspilerService as NewTranspilerService
 
 logger = logging.getLogger(__name__)
 from warnings import warn
 
 
-class TranspilerService:
-    """Class for using the transpiler service.
+class TranspilerService(NewTranspilerService):
+    """Class for using the transpiler service. Deprecated. Use qiskit_ibm_transpiler package instead
 
     :param optimization_level: The optimization level to use during the transpilation. There are 4 optimization levels ranging from 0 to 3, where 0 is intended for not performing any optimizations and 3 spends the most effort to optimize the circuit.
     :type optimization_level: int
@@ -71,50 +69,12 @@ class TranspilerService:
             DeprecationWarning,
         )
 
-        self.transpiler_service = TranspileAPI(**kwargs)
-
-        self.backend_name = backend_name
-        self.coupling_map = coupling_map
-        self.optimization_level = optimization_level
-        self.ai = ai
-        self.qiskit_transpile_options = qiskit_transpile_options
-
-        if ai_layout_mode is not None:
-            if ai_layout_mode.upper() not in ["KEEP", "OPTIMIZE", "IMPROVE"]:
-                raise (
-                    f"ERROR. Unknown ai_layout_mode: {ai_layout_mode.upper()}. Valid modes: 'KEEP', 'OPTIMIZE', 'IMPROVE'"
-                )
-            self.ai_layout_mode = ai_layout_mode.upper()
-        else:
-            self.ai_layout_mode = ai_layout_mode
-        super().__init__()
-
-    def run(
-        self,
-        circuits: Union[List[Union[str, QuantumCircuit]], Union[str, QuantumCircuit]],
-    ):
-        """Transpile the circuit(s) by calling the service /transpile endpoint.
-
-        Args:
-            circuits: circuit(s) to transpile.
-
-        Returns:
-            The transpiled circuit(s)
-
-        """
-        logger.info(f"Requesting transpile to the service")
-        transpile_result = self.transpiler_service.transpile(
-            circuits=circuits,
-            backend=self.backend_name,
-            coupling_map=self.coupling_map,
-            optimization_level=self.optimization_level,
-            ai=self.ai,
-            qiskit_transpile_options=self.qiskit_transpile_options,
-            ai_layout_mode=self.ai_layout_mode,
+        super().__init__(
+            optimization_level,
+            ai,
+            coupling_map,
+            backend_name,
+            qiskit_transpile_options,
+            ai_layout_mode,
+            **kwargs,
         )
-        if transpile_result is None:
-            logger.warning("Qiskit IBM Transpiler couldn't transpile the circuit(s)")
-            return None
-
-        logger.info("Qiskit IBM Transpiler returned a result")
-        return transpile_result
