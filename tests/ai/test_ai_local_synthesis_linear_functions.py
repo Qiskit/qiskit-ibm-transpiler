a6c8dd36c1b7a3c471ff4252fac238b27e4505fc
Yaiza <yaiza.garcia.mm@gmail.com>
2024-10-16 16:57:03 +0200

    Adds and clean tests

--- tests/ai/test_ai_local_synthesis_linear_functions.py
+++ tests/ai/test_ai_local_synthesis_linear_functions.py
@@ -14,106 +14,28 @@
 import pytest
 from qiskit import QuantumCircuit
 from qiskit.transpiler import PassManager
-from qiskit.quantum_info import Operator
-import numpy as np
 
 from qiskit_ibm_transpiler.ai.collection import CollectLinearFunctions
 from qiskit_ibm_transpiler.ai.synthesis import AILinearFunctionSynthesis
 
 
-# def test_linear_function_wrong_backend(random_circuit_transpiled, caplog):
-#     ai_optimize_lf = PassManager(
-#         [
-#             CollectLinearFunctions(),
-#             AILinearFunctionSynthesis(backend_name="wrong_backend"),
-#         ]
-#     )
-#     ai_optimized_circuit = ai_optimize_lf.run(random_circuit_transpiled)
-#     assert "couldn't synthesize the circuit" in caplog.text
-#     assert "Keeping the original circuit" in caplog.text
-#     assert (
-#         "User doesn't have access to the specified backend: wrong_backend"
-#         in caplog.text
-#     )
-#     assert isinstance(ai_optimized_circuit, QuantumCircuit)
+def test_linear_function_synthesis_wrong_backend():
+    original_circuit = QuantumCircuit(3)
+    original_circuit.cx(0, 1)
+    original_circuit.cx(1, 2)
 
+    ai_linear_functions_synthesis_pass = PassManager(
+        [
+            CollectLinearFunctions(min_block_size=2),
+            AILinearFunctionSynthesis(backend_name="wrong_backend"),
+        ]
+    )
 
-# @pytest.mark.skip(
-#     reason="Unreliable. It passes most of the times with the timeout of 1 second for the current circuits used"
-# )
-# def test_linear_function_exceed_timeout(random_circuit_transpiled, backend, caplog):
-#     ai_optimize_lf = PassManager(
-#         [
-#             CollectLinearFunctions(),
-#             AILinearFunctionSynthesis(backend_name=backend, timeout=1),
-#         ]
-#     )
-#     ai_optimized_circuit = ai_optimize_lf.run(random_circuit_transpiled)
-#     assert "couldn't synthesize the circuit" in caplog.text
-#     assert "Keeping the original circuit" in caplog.text
-#     assert isinstance(ai_optimized_circuit, QuantumCircuit)
-
-
-# @pytest.mark.skip(
-#     reason="Unreliable many times. We'll research why it fails sporadically"
-# )
-# def test_linear_function_wrong_token(random_circuit_transpiled, backend, caplog):
-#     ai_optimize_lf = PassManager(
-#         [
-#             CollectLinearFunctions(),
-#             AILinearFunctionSynthesis(backend_name=backend, token="invented_token_2"),
-#         ]
-#     )
-#     ai_optimized_circuit = ai_optimize_lf.run(random_circuit_transpiled)
-#     assert "couldn't synthesize the circuit" in caplog.text
-#     assert "Keeping the original circuit" in caplog.text
-#     assert "Invalid authentication credentials" in caplog.text
-#     assert isinstance(ai_optimized_circuit, QuantumCircuit)
-
-
-# @pytest.mark.skip(
-#     reason="Unreliable many times. We'll research why it fails sporadically"
-# )
-# @pytest.mark.disable_monkeypatch
-# def test_linear_function_wrong_url(random_circuit_transpiled, backend):
-#     ai_optimize_lf = PassManager(
-#         [
-#             CollectLinearFunctions(),
-#             AILinearFunctionSynthesis(
-#                 backend_name=backend, base_url="https://ibm.com/"
-#             ),
-#         ]
-#     )
-#     try:
-#         ai_optimized_circuit = ai_optimize_lf.run(random_circuit_transpiled)
-#         pytest.fail("Error expected")
-#     except Exception as e:
-#         assert "Expecting value: line 1 column 1 (char 0)" in str(e)
-#         assert type(e).__name__ == "JSONDecodeError"
-
-
-# @pytest.mark.skip(
-#     reason="Unreliable many times. We'll research why it fails sporadically"
-# )
-# @pytest.mark.disable_monkeypatch
-# def test_linear_function_unexisting_url(random_circuit_transpiled, backend, caplog):
-#     ai_optimize_lf = PassManager(
-#         [
-#             CollectLinearFunctions(),
-#             AILinearFunctionSynthesis(
-#                 backend_name=backend,
-#                 base_url="https://invented-domain-qiskit-ibm-transpiler-123.com/",
-#             ),
-#         ]
-#     )
-#     ai_optimized_circuit = ai_optimize_lf.run(random_circuit_transpiled)
-#     assert "couldn't synthesize the circuit" in caplog.text
-#     assert "Keeping the original circuit" in caplog.text
-#     assert (
-#         "Error: HTTPSConnectionPool(host='invented-domain-qiskit-ibm-transpiler-123.com', port=443):"
-#         in caplog.text
-#     )
-#     assert isinstance(ai_optimized_circuit, QuantumCircuit)
+    with pytest.raises(
+        PermissionError,
+        match=r"ERROR. Backend not supported \(\w+\)",
+    ):
+        ai_linear_functions_synthesis_pass.run(original_circuit)
 
 
 def test_linear_function_synthesis_returns_original_circuit(caplog):
