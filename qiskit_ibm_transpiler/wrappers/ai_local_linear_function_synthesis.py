5471d5dd255c2891c749535ff0684835b8bbf96b
Yaiza <yaiza.garcia.mm@gmail.com>
2024-11-06 14:33:50 +0100

    Deletes unneeded code

--- qiskit_ibm_transpiler/wrappers/ai_local_linear_function_synthesis.py
+++ qiskit_ibm_transpiler/wrappers/ai_local_linear_function_synthesis.py
@@ -11,24 +11,13 @@
 # that they have been altered from the originals.
 
 import logging
-import networkx as nx
-from networkx.exception import NetworkXError
 
 from qiskit_ibm_ai_local_transpiler import AILinearFunctionInference
 
 from typing import Union, List
 
-import numpy as np
 from qiskit import QuantumCircuit
-from qiskit.transpiler import CouplingMap
 from qiskit.circuit.library import LinearFunction
-from qiskit.quantum_info import Clifford
-from qiskit_ibm_runtime import QiskitRuntimeService
-from qiskit_ibm_transpiler.ai.models.linear_functions import (
-    model_coupling_map_by_model_hash,
-    MODEL_HASHES as MODEL_LIN_FUNC_HASHES,
-)
-from qiskit_ibm_transpiler.utils import get_qasm_from_circuit
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -74,126 +63,20 @@ class AILocalLinearFunctionSynthesis:
                 f"and the number of input qargs arrays {n_qargs} are different."
             )
 
-        clifford_dict = [Clifford(circuit).to_dict() for circuit in circuits]
-
-        coupling_map_graph = get_coupling_map_graph(backend_name, coupling_map)
-
         logger.info("Running Linear Functions AI synthesis on local mode")
 
-        synthesized_circuits = get_synthesized_linear_function_circuits(
-            coupling_map_graph, clifford_dict, qargs
-        )
+        synthesized_circuits = []
+
+        for index, circuit_qargs in enumerate(qargs):
+
+            synthesized_linear_function = AILinearFunctionInference().synthesize(
+                circuit=circuits[index],
+                coupling_map=coupling_map,
+                circuit_qargs=circuit_qargs,
+            )
+
+            synthesized_circuits.append(synthesized_linear_function)
+
+        logger.info("Linear Functions AI synthesis on local mode completed")
 
         return synthesized_circuits
-
-
-def perm_cliff(cliff, perm):
-    perm = np.array(perm)
-    cliff.stab_x = cliff.stab_x[:, perm]
-    cliff.stab_z = cliff.stab_z[:, perm]
-    cliff.destab_x = cliff.destab_x[:, perm]
-    cliff.destab_z = cliff.destab_z[:, perm]
-    cliff.stab = cliff.stab[perm, :]
-    cliff.destab = cliff.destab[perm, :]
-    return cliff
-
-
-def get_synthesized_linear_function_circuits(
-    coupling_map: nx.Graph, clifford_dicts: List[dict], qargs: List[List[int]]
-) -> list[QuantumCircuit]:
-    synthesized_circuits = []
-
-    for index, circuit_qargs in enumerate(qargs):
-        try:
-            subgraph_perm, cmap_hash = get_mapping_perm(coupling_map, circuit_qargs)
-        except BaseException:
-            raise AttributeError(f"ERROR. Malformed qargs {circuit_qargs}")
-
-        # Generate the Clifford from the dictionary to send it to the model and permute it
-        clifford = perm_cliff(Clifford.from_dict(clifford_dicts[index]), subgraph_perm)
-
-        synthesized_linear_function = AILinearFunctionInference().synthesize(
-            cliff=clifford, coupling_map_hash=cmap_hash
-        )
-
-        # Permute the circuit back
-        synthesized_circuit = QuantumCircuit(
-            synthesized_linear_function.num_qubits
-        ).compose(synthesized_linear_function, qubits=subgraph_perm)
-
-        # synthesized_circuit could be None or have a value, we return it in both cases
-        synthesized_circuits.append(synthesized_circuit)
-
-    return synthesized_circuits
-
-
-def get_coupling_map_graph(
-    backend_name: str | None = None, backend_coupling_map: list[list[int]] | None = None
-) -> nx.Graph:
-    coupling_map_list_format = None
-
-    if backend_coupling_map:
-        coupling_map_list_format = backend_coupling_map
-    elif backend_name:
-        try:
-            runtime_service = QiskitRuntimeService()
-            backend_info = runtime_service.backend(name=backend_name)
-            coupling_map_edges = CouplingMap.get_edges(backend_info.coupling_map)
-            coupling_map_list_format = [list(edge) for edge in coupling_map_edges]
-        except Exception:
-            raise PermissionError(f"ERROR. Backend not supported ({backend_name})")
-
-    try:
-        coupling_map = nx.Graph(coupling_map_list_format)
-    except Exception:
-        raise NetworkXError("ERROR. Cannot convert coupling_map from list to graph")
-
-    return coupling_map
-
-
-def get_mapping_perm(coupling_map: nx.Graph, circuit_qargs: List[int]) -> list[int]:
-    # Identify the subgraph of the device coupling map where the circuit is.
-    circuit_in_coupling_map = coupling_map.subgraph(circuit_qargs)
-
-    is_circuit_in_coupling_map_connected = nx.is_connected(circuit_in_coupling_map)
-
-    if not is_circuit_in_coupling_map_connected:
-        raise ValueError(
-            "ERROR. Qargs do not form a connected subgraph of the backend coupling map"
-        )
-
-    # We find which model to use by hashing the circuit subgraph
-    circuit_in_coupling_map_hash = nx.weisfeiler_lehman_graph_hash(
-        circuit_in_coupling_map
-    )
-
-    # If there is no model for that circuit_in_coupling_map, we cannot use AI.
-    if circuit_in_coupling_map_hash not in MODEL_LIN_FUNC_HASHES:
-        raise LookupError("ERROR. No model available for the requested subgraph")
-
-    model_coupling_map = model_coupling_map_by_model_hash[circuit_in_coupling_map_hash]
-
-    # circuit_in_coupling_map could appears several times on the model's topology. We get the first
-    # we find due to the `next` function. device_model_mapping contains a nodes correspondency between
-    # the device and the model
-    device_model_mapping = next(
-        iter(
-            nx.isomorphism.GraphMatcher(
-                circuit_in_coupling_map, nx.Graph(model_coupling_map)
-            ).match()
-        )
-    )
-
-    # We now have to find the permutation that we should apply to the Clifford based on the mapping we found
-    qargs_dict = {v: i for i, v in enumerate(circuit_qargs)}
-
-    # subgraph_perm will be a list where each position indicates a permutation. The index refers to the circuit
-    # qubit and the value refers to the qubit on the model
-    subgraph_perm = [
-        qargs_dict[v]
-        for v in sorted(
-            device_model_mapping.keys(), key=lambda k: device_model_mapping[k]
-        )
-    ]
-
-    return subgraph_perm, circuit_in_coupling_map_hash
