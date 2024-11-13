efac5bb63f728092d6ac2af738bd43c0d6e77eba
Jesus Talavera <jesus.talavera@ibm.com>
2024-11-13 18:05:56 +0100

    Preparing release qiskit-ibm-transpiler 0.8.1

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, Path(__file__).parent.resolve())
 project = "Qiskit IBM Transpiler"
 copyright = "2024, IBM Quantum"
 author = "IBM Quantum"
-release = "0.8.0"
+release = "0.8.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
