45f42fee35f4e7d81bed888eb6e3651ac0a3940c
Jesus Talavera <jesus.talavera@ibm.com>
2025-03-17 13:17:40 +0100

    Upgrade QPY version mapping

--- qiskit_ibm_transpiler/utils.py
+++ qiskit_ibm_transpiler/utils.py
@@ -47,7 +47,11 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 logger = logging.getLogger(__name__)
 
 QPY_QISKIT_VERSION_MAPPING = {
-    "1.4.0": 13,
+    "2.0.0": 20,
+    "1.4.0": 14,
+    "1.4.1": 14,
+    "1.4.1.1": 14,
+    "1.4.2": 14,
     "1.3.0": 13,
     "1.3.1": 13,
     "1.3.2": 13,
